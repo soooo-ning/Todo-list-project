3c6f17e54d036692a3a099e8f6b5c25f6ac2cbbf
lychee-55 <zylee503@naver.com>
2024-12-19 17:21:11 +0900

    프로필설정,비번,회원탈퇴,네비바 ejs파일 업로드-routes.index링크 수정

---
+++ controllers/UserController.js
@@ -0,0 +1,15 @@
+const models = require('../models/User');
+
+exports.getProfile = (req, res) => {
+  res.render('profile_setting');
+};
+exports.editProfile = (req, res) => {};
+exports.getResetPw = (req, res) => {
+  res.render('change_pw');
+};
+exports.resetPw = (req, res) => {};
+
+exports.getDeleteAccount = (req, res) => {
+  res.render('delete_account');
+};
+exports.deleteAccount = (req, res) => {};

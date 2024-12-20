5707434bb84ebb42a07b9fa9446b0df913917157
baekdahyun <wpghekgus3030@gmail.com>
2024-12-20 18:38:02 +0900

    화면완성

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const { Keyword, Todo, TodoContent, User } = require('../models');
+const { Op } = require('sequelize');
+const { success, serverError, notFound } = require('../utils/common');
+
 exports.getSignIn = (req, res) => {
   res.render('sign-in');
 };
@@ -6,4 +10,42 @@ exports.getSignUp = (req, res) => {
 };
 exports.getSearchPw = (req, res) => {
   res.render('search-pw');
+};
+
+// POST /auth/api/sign-up 회원가입
+exports.signUp = (req, res) => {
+  models.User.create({
+    nickname: req.body.userName,
+    pw: req.body.userPw,
+    email: req.body.userEmail,
+  })
+    .then((result) => {
+      res.send(result);
+      console.log(req.body.userEmail);
+    })
+    .catch((err) => {
+      errorlogs(
+        err,
+        'SignUp Error',
+        '회원가입 중 오류가 발생했습니다.',
+        500,
+        res,
+      );
+    });
+};
+
+// POST /auth/api/sign-in 로그인
+exports.signIn = async (req, res) => {
+  try {
+    const result = await models.User.findOne({
+      where: {
+        pw: req.body.userPw,
+        email: req.body.userEmail,
+      },
+    });
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('서버 에러가 발생했습니다.');
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,44 +4,44 @@ const router = express.Router();
 // 컨트롤러를 각각 선언
 const authController = require('../controllers/AuthController');
 const todoController = require('../controllers/TodoController');
-const userController = require('../controllers/UserController');
+// const userController = require('../controllers/UserController');
 
 // Auth signin
 router.get('/', authController.getSignIn);
 router.post('/auth/api/sign-in', authController.signIn);
-router.post('/auth/api/sign-in/token', authController.jwtToken);
-router.post('/auth/api/sign-in/google', authController.signInGoogle);
-router.post('/auth/api/sign-in/kakao', authController.signInKakao);
+// router.post('/auth/api/sign-in/token', authController.jwtToken);
+// router.post('/auth/api/sign-in/google', authController.signInGoogle);
+// router.post('/auth/api/sign-in/kakao', authController.signInKakao);
 
 // Auth signup
 router.get('/auth/sign-up', authController.getSignUp);
 router.post('/auth/api/sign-up', authController.signUp);
-router.get('/auth/api/sign-up/check', authController.duplicatedEmail);
+// router.get('/auth/api/sign-up/check', authController.duplicatedEmail);
 
 // Auth search pw
 router.get('/auth/search-pw', authController.getSearchPw);
-router.get('/auth/api/search-pw', authController.searchPw);
+// router.get('/auth/api/search-pw', authController.searchPw);
 
-// User
-router.get('/user/profile', userController.getProfile);
-router.patch('/user/api/profile', userController.editProfile);
-router.get('/user/reset-pw', userController.getResetPw);
-router.patch('/user/api/reset-pw', userController.resetPw);
-router.get('/user/delete-account', userController.getDeleteAccount);
-router.delete('/user/api/delete-account', userController.deleteAccount);
+// // User
+// router.get('/user/profile', userController.getProfile);
+// router.patch('/user/api/profile', userController.editProfile);
+// router.get('/user/reset-pw', userController.getResetPw);
+// router.patch('/user/api/reset-pw', userController.resetPw);
+// router.get('/user/delete-account', userController.getDeleteAccount);
+// router.delete('/user/api/delete-account', userController.deleteAccount);
 
 // Todo CRUD
 router.get('/todo/api/write', todoController.getWriteTodo);
-router.post('/todo/api/write', todoController.writeTodo);
-router.get('/todo/api/get', todoController.getTodo);
-router.patch('/todo/api/content', todoController.editTodo);
-router.patch('/todo/api/state', todoController.updateState);
-router.delete('/todo/api/delete', todoController.deleteTodo);
+// router.post('/todo/api/write', todoController.writeTodo);
+// router.get('/todo/api/get', todoController.getTodo);
+// router.patch('/todo/api/content', todoController.editTodo);
+// router.patch('/todo/api/state', todoController.updateState);
+// router.delete('/todo/api/delete', todoController.deleteTodo);
 
-router.get('/todo/api/search', todoController.searchTodo);
-router.get('/todo/api/calander', todoController.calendarList);
-router.get('/todo/api/list/priority', todoController.priorityList);
-router.get('/todo/api/list/keyword', todoController.keywordList);
-router.get('/todo/api/deleted-todo', todoController.deleteList);
+// router.get('/todo/api/search', todoController.searchTodo);
+// router.get('/todo/api/calander', todoController.calendarList);
+// router.get('/todo/api/list/priority', todoController.priorityList);
+// router.get('/todo/api/list/keyword', todoController.keywordList);
+// router.get('/todo/api/deleted-todo', todoController.deleteList);
 
 module.exports = router;

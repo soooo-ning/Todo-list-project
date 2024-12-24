6b1b8130d32102a9f13a4d2ffa99a07edcf15f82
lychee-55 <zylee503@naver.com>
2024-12-24 17:44:04 +0900

    css 수정, 프로필설정 뷰파일 및 네비 뷰 변경,routes에 미들웨어 하나 추가

---
+++ middlewares/userData.js
@@ -0,0 +1,25 @@
+const { User, Keyword } = require('../models');
+
+const loadUserData = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { id: 1 },
+      attributes: ['nickname', 'profile_image'],
+    });
+
+    const keywords = await Keyword.findAll({
+      attributes: ['keyword'],
+    });
+
+    res.locals.nickname = user.nickname;
+    res.locals.profile_img = user.profile_image;
+    res.locals.keywords = keywords;
+
+    next();
+  } catch (error) {
+    console.error('Error in loadUserData middleware:', error);
+    next(error);
+  }
+};
+
+module.exports = loadUserData;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const uploadDetail = require('../middlewares/multer');
+const loadUserData = require('../middlewares/userData');
 
 const authController = require('../controllers/AuthController');
 const todoController = require('../controllers/TodoController');
@@ -31,7 +32,7 @@ router.get('/auth/search-pw', authController.getSearchPw); // 비밀번호 페�
 // router.get('/auth/api/search-pw', authController.searchPw); // 비밀번호 찾기 api
 
 // User
-router.get('/user/profile', userController.getProfile); // 프로필 페이지
+router.get('/user/profile', loadUserData, userController.getProfile); // 프로필 페이지
 router.patch('/user/api/profile', userController.editProfile); // 프로필 수정 api
 router.post(
   '/user/api/photo-upload',

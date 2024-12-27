b2d81d3a083b36878e67bb95d82e116d446af1b1
lychee-55 <zylee503@naver.com>
2024-12-27 11:40:38 +0900

    네비바 수정 및 미들웨어 수정(id찾기 연동 전)

--- SQL/DCL.sql
+++ SQL/DCL.sql
@@ -1,4 +1,4 @@
--- Active: 1732688616928@@127.0.0.1@3306@todo
+-- Active: 1732688863205@@127.0.0.1@3306@todo
 -- 개발 계정
 CREATE USER 'developer'@'localhost' IDENTIFIED BY 'todo';
 
--- middlewares/userData.js
+++ middlewares/userData.js
@@ -2,19 +2,22 @@ const { User, Keyword } = require('../models');
 
 const loadUserData = async (req, res, next) => {
   try {
+    const token = req.headers.authorization?.split(' ')[1];
+    console.log(token);
     const user = await User.findOne({
       where: { id: 1 },
-      attributes: ['nickname', 'profile_image'],
+      attributes: ['nickname', 'profile_image', 'id'],
     });
 
     const keywords = await Keyword.findAll({
       where: { user_id: 1 },
-      attributes: ['keyword'],
+      attributes: ['id', 'keyword', 'user_id'], // ID 포함
     });
 
     res.locals.nickname = user.nickname;
     res.locals.profile_img = user.profile_image;
-    res.locals.keywords = keywords;
+    res.locals.keywords = keywords; // 그대로 배열 전달
+    res.locals.userId = user.id; // 명확하게 변수명 지정
 
     next();
   } catch (error) {
